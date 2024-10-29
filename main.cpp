c6c99ba3ebcb581496af392ad798d526a0d48d1e
Adrian <chiriac.adrian.q7w@student.ucv.ro>
2024-10-29 18:03:04 +0200

    commit de la curs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+struct Doctor
+{
+    string drId;
+    string speciality;
+};
+
+struct Patient
+{
+    string problem;
+    string speciality;
+};
 
 int main()
 {
@@ -10,24 +23,35 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+    vector<Patient>injuries;
+    vector<Doctor>doctors;
+
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        injuries.push_back({name, speciality});
+        cout << injuries[i].problem << " " << injuries[i].speciality << " " << i << "\n";
     }
 
     inFile >> no_doctors;
-
+    cout << "\n";
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctors.push_back({name, speciality});
+        cout << doctors[i].drId<< " " << doctors[i].speciality << " " << i << "\n";
     }
+    /*
+    for (int i = 0; i < no_problems; i++)
+    {
+
+
+    }*/
 
     return 0;
 }
