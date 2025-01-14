5c22d21552f21966a9a080fee4ddee222dcd327e
Adrian <chiriac.adrian.q7w@student.ucv.ro>
2025-01-14 17:24:36 +0200

    solved problem STL 4

--- main.cpp
+++ main.cpp
@@ -1,84 +1,108 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <string>
-#include <queue>
+#include<set>
+
 using namespace std;
 
-ifstream fin("input.txt");
 
-struct Doctor
+struct problem
 {
-	string Name;
-	vector<string>Specialties;
-	int liber_de_la=8;
-	vector<pair<string, int>>Answer; // .first=specialty .second=when they arrive
+    string id, speciality;
+    int arrivalTime, cureTime, priority;
 };
 
-struct Problem
-{
-	string Name;
-	string SpecialtyNeeded;
-	int arrival_time;
-	int duration_of_treatment;
-	int priority;
-
-	bool operator<(const Problem& other) const 
-	{
-		// logic here
-		if (arrival_time != other.arrival_time)
-			return arrival_time > other.arrival_time;
-		return priority < other.priority;
-	}
+struct doctor {
+    string id;
+    int nr_specialities, freeAfter = 9;
+    set<string> specialities;
+    vector<pair<string, int>> cured;
 };
 
 int main()
 {
-	int HowManyProblems;
-	priority_queue<Problem>Problems;
-	int HowManyDoctors;
-	vector<Doctor>Doctors;
+    ifstream inFile("input2.txt");
 
-	fin >> HowManyProblems;
-	for (int i = 1; i <= HowManyProblems; i++)
-	{
-		Problem p;
-		fin >> p.Name >> p.SpecialtyNeeded >> p.arrival_time >> p.duration_of_treatment >> p.priority;
-		Problems.push(p);
-	}
+    int no_problems, no_doctors;
 
-	fin >> HowManyDoctors;
-	for (int i = 1; i <= HowManyDoctors; i++)
-	{
-		int HowManySpecialties;
-		Doctor d;
-		fin >> d.Name;
-		fin >> HowManySpecialties;
-		for (int j = 1; j <= HowManySpecialties; j++)
-		{
-			string spec;
-			fin >> spec;
-			d.Specialties.push_back(spec);
-		}
-		d.liber_de_la = 8;
 
-		Doctors.push_back(d);
-	}
+    inFile >> no_problems;
+    vector<struct problem> prb_list;
 
-	//Afisare probleme sortate in pq
-	cout << "Afisare pq:\n";
-	cout << Problems.size() << "\n";
-	while (!Problems.empty())
-	{
-		Problem p = Problems.top();
-		cout << p.Name << " " << p.SpecialtyNeeded << " " << p.arrival_time << " " << p.duration_of_treatment << " " << p.priority << "\n";
-		Problems.pop();
-	}
+    for (int i = 0; i < no_problems; i++)
+    {
+        struct problem prb;
+        inFile >> prb.id;
+        inFile >> prb.speciality;
+        inFile >> prb.arrivalTime;
+        inFile >> prb.cureTime;
+        inFile >> prb.priority;
+        //cout << prb.id << ' ' << prb.speciality << '\n';
+        prb_list.push_back(prb);
+    }
 
-	//Rezolvarea problemei...
-	
-	//se mai putea si cu map de specializari 
+    inFile >> no_doctors;
 
-	return 0;
+    vector<struct doctor> doc_list;
+
+    for (int i = 0; i < no_doctors; i++)
+    {
+        struct doctor doc;
+        inFile >> doc.id;
+        inFile >> doc.nr_specialities;
+        for (int i = 0; i < doc.nr_specialities; i++)
+        {
+            string speciality;
+            inFile >> speciality;
+            doc.specialities.insert(speciality);
+        }
+        // cout << doc.id << ' ' << doc.nr_specialities << '\n';
+        doc_list.push_back(doc);
+    }
+
+    sort(prb_list.begin(), prb_list.end(), [](auto& prb1, auto& prb2)
+        {
+            if (prb1.arrivalTime == prb2.arrivalTime)
+                return prb1.priority > prb2.priority;
+            return prb1.arrivalTime < prb2.arrivalTime;
+        });
+
+    //DEBUG: problem list after sorting
+    /*
+    for (auto problem : prb_list)
+    {
+        cout << problem.arrivalTime << " " << problem.speciality << " si prioritate " << problem.priority << endl;
+    }
+    cout << endl;
+    */
+
+    for (auto& prb : prb_list)
+    {
+        auto doctor = find_if(doc_list.begin(), doc_list.end(), [&](struct doctor doc) {
+            return doc.specialities.contains(prb.speciality) && doc.freeAfter <= prb.arrivalTime && doc.freeAfter + prb.cureTime <=17;
+            });
+        if (doctor != doc_list.end())
+        {
+            ///DEBUG: affirmative finding of doctor who can solve the problem
+            //cout << "s-a gasit doctorul" << doctor->id << "si rezolva problema " << prb.id << endl;
+            doctor->freeAfter += prb.cureTime;
+            doctor->cured.push_back(pair<string, int>(prb.id, prb.arrivalTime));
+        }
+    }
+    for (auto& doc : doc_list)
+    {
+        if (!doc.cured.empty())
+        {
+        cout << doc.id << " ";
+        for (auto& sth : doc.cured)
+        {
+            cout << sth.first << " " << sth.second << " ";
+        }
+        cout << endl;
+        }
+    }
+
+    return 0;
 }
